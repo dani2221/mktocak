6953a53435f19e3eb32f4a69fcc142b12a2dd21e
onitwin <onitwin@gmail.com>
2021-01-19 19:05:34 +0000

    minor test change

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -8,7 +8,7 @@ class Welcome extends Component {
     return (
       <div className="App" style={{textAlign:'center',padding:'50px'}}>
 
-        <h1 className='h1' style={{margin:'30px'}}>Get Involved!</h1>
+        <h1 className='h1' style={{margin:'30px'}}>Josephs Change!</h1>
         <h3 style={{margin:'30px'}}>Submit new paths and places</h3>
         <a href='/contribute' className='btn draw-border' style={{width:'150px',margin:'auto',textDecoration:'none'}}>Join</a>
         <p><i>and collect cycle points.</i></p>
